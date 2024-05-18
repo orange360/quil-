e5041a76bc0ca718c357560215dfc685b9aefcac
orange360 <52391316+orange360@users.noreply.github.com>
2024-05-18 16:19:29 +0800

    Update and rename control001.py to control.py

--- control.py
+++ control.py
@@ -1,12 +1,23 @@
 import requests
-
 import paramiko
 import json
 import pandas as pd
+import logging
+
+
+# Configure logging for the script
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[logging.FileHandler('output.log', 'a', 'utf-8'),
+                              logging.StreamHandler()])
+
+# Set paramiko logging level to WARNING to suppress INFO messages
+paramiko.util.log_to_file('paramiko.log', level='WARNING')
+
+logger = logging.getLogger(__name__)
 
 def send_to_dingtalk_webhook(message):
-    # 填入你的钉钉webhook
-    webhook= ''
+    webhook = ''
     if not webhook:  # 如果webhook为空，则不执行后续代码
         return
     webhook_url = webhook
@@ -20,17 +31,14 @@ def send_to_dingtalk_webhook(message):
 
     try:
         response = requests.post(webhook_url, headers=headers, json=data)
-        # print(message)
-        # print(webhook_url)
         if response.status_code == 200:
-            print("消息已成功发送到钉钉机器人！")
-            # print(response)
+            logger.info("消息已成功发送到钉钉机器人！")
         else:
-            print(f"发送消息到钉钉机器人失败，错误代码：{response.status_code}")
+            logger.error(f"发送消息到钉钉机器人失败，错误代码：{response.status_code}")
     except requests.exceptions.RequestException as e:
-        print(f"发生异常：{e}")
+        logger.error(f"发生异常：{e}")
     except Exception as e:
-        print(f"发生异常：{e}")
+        logger.error(f"发生异常：{e}")
 
 def ssh_connect(hostname, port, username, password):
     try:
@@ -39,7 +47,7 @@ def ssh_connect(hostname, port, username, password):
         ssh.connect(hostname, port, username, password)
         return ssh
     except Exception as e:
-        print(f"An error occurred while connecting: {str(e)}")
+        logger.error(f"An error occurred while connecting: {str(e)}")
         return None
 
 def execute_command(ssh, command):
@@ -49,22 +57,22 @@ def execute_command(ssh, command):
         error = stderr.read().decode()
         return output, error
     except Exception as e:
-        print(f"An error occurred while executing command: {str(e)}")
+        logger.error(f"An error occurred while executing command: {str(e)}")
         return None, None
 
 def install_quil(ssh):
     command = 'sudo -i wget albert.vip/auto_install_quil_and_initia_reboot.sh && chmod +x auto_install_quil_and_initia_reboot.sh && ./auto_install_quil_and_initia_reboot.sh'
     output, error = execute_command(ssh, command)
-    print(output)
+    logger.info(output)
     if error:
-        print(f"Error: {error}")
+        logger.error(f"Error: {error}")
 
 def clone_store_repo(ssh):
     command = 'sudo -i rm -rf /www/ceremonyclient/node/.config/store/ && cd /www/ceremonyclient/node/.config/ && git clone https://github.com/a154225859/store.git'
     output, error = execute_command(ssh, command)
-    print(output)
+    logger.info(output)
     if error:
-        print(f"Error: {error}")
+        logger.error(f"Error: {error}")
 
 def run_check_script(ssh, name):
     command = 'sudo grpcurl -plaintext localhost:8337 quilibrium.node.node.pb.NodeService.GetNodeInfo'
@@ -77,41 +85,37 @@ def run_check_script(ssh, name):
             peerId = output_json.get("peerId")
             max_frame = output_json.get("maxFrame")
             if max_frame:
-                print(f"name: {name}")
-                print(f"peerId: {peerId}")
-                print(f"maxFrame: {max_frame}\n")
+                logger.info(f"name: {name}")
+                logger.info(f"peerId: {peerId}")
+                logger.info(f"maxFrame: {max_frame}\n")
         except json.JSONDecodeError:
             reboot(ssh, name)
-            print(f"{name}挂了: {output}", end='')
+            logger.info(f"{name}挂了: {output}", end='')
 
     if error:
-        print(f"{name}挂了: \n{error}", end='')
+        logger.error(f"{name}挂了: \n{error}", end='')
         reboot(ssh, name)
-
 
 def run_start_script(ssh):
     command = 'sudo cd /root/ && ./start.sh'
     output, error = execute_command(ssh, command)
-    print(output)
+    logger.info(output)
     if error:
-        print(f"Error: {error}")
+        logger.error(f"Error: {error}")
 
-# 从xlsx文件读取服务器信息
 def read_server_info(file_path):
     df = pd.read_excel(file_path)
     servers = df.to_dict(orient='records')
     return servers
 
-# 重启服务器
 def reboot(ssh, name):
     message = f"{name}挂了，进行重启"
     send_to_dingtalk_webhook(message)
-    print(f"{name}已经重启\n")
+    logger.info(f"{name}已经重启\n")
     command = 'sudo reboot'
     output, error = execute_command(ssh, command)
     if error:
-        print(f"Error: {error}")
-
+        logger.error(f"Error: {error}")
 
 def check_grpcurl_installed(ssh):
     check_command = 'which grpcurl'
@@ -119,9 +123,7 @@ def check_grpcurl_installed(ssh):
     return output.strip() != ""
 
 def install_grpcurl(ssh):
-    # 检查 grpcurl 是否已安装
     if not check_grpcurl_installed(ssh):
-        # 安装 grpcurl
         install_command = (
             'wget https://github.com/fullstorydev/grpcurl/releases/download/v1.8.7/grpcurl_1.8.7_linux_x86_64.tar.gz && '
             'tar -xvf grpcurl_1.8.7_linux_x86_64.tar.gz && '
@@ -129,13 +131,11 @@ def install_grpcurl(ssh):
             'rm grpcurl_1.8.7_linux_x86_64.tar.gz'
         )
         output, error = execute_command(ssh, install_command)
-        print("Installing grpcurl Output:", output)
-        print("Installing grpcurl Error:", error)
+        logger.info("Installing grpcurl Output:", output)
+        logger.error("Installing grpcurl Error:", error)
     else:
-        print("grpcurl is already installed.")
+        logger.info("grpcurl is already installed.")
 
-
-# 配置服务器信息文件路径
 file_path = 'server_info.xlsx'
 servers = read_server_info(file_path)
 
@@ -146,13 +146,10 @@ for server in servers:
     username = server['username']
     password = server['password']
 
-    # 连接服务器
     ssh = ssh_connect(hostname, port, username, password)
 
     if ssh:
-        # 根据需要调用不同的函数
-        # install_grpcurl(ssh)# 第一次运行没有grp时候，运行一次，后面关掉
+        # Uncomment the line below to install grpcurl if not already installed
+        # install_grpcurl(ssh)
         run_check_script(ssh, name)
-
-        # 关闭连接
         ssh.close()
